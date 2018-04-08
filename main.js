360cec4bd3baaa304312d62e23c3dce6caec7547
Nathaniel Rubin <nateboop@gmail.com>
2018-04-08 13:29:54 -0400

    Don't open window on startup

--- main.js
+++ main.js
@@ -7,36 +7,14 @@ const WallpaperManager = require('./src/wallpaper_manager.js')
 // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
+function runApp () {
   WallpaperManager.run();
-
-  // Create the browser window.
-  // win = new BrowserWindow({width: 800, height: 600, webPreferences: { devTools: false }})
-  win = new BrowserWindow({width: 800, height: 600});
-
-  // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // win.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', runApp)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -51,7 +29,7 @@ app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    runApp()
   }
 })
 
